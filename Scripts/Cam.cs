9b168680d7b76e46ab94a00cbe0af122f07bf9a1
Yuanguo-notebook <45431499+Yuanguo-notebook@users.noreply.github.com>
2019-09-11 00:33:48 -0400

    raycasthit from camera to cube
     and print coordinates of hitting point on cube

---
+++ Scripts/Cam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour {
+
+	Camera cam;
+
+	private Vector3 forwardDir;
+	Vector3 dir;
+	void Start()
+	{
+		cam = GetComponent<Camera>();
+
+		 dir = new Vector3(0,0,1.0f);
+
+	}
+
+	void Update()
+	{
+		forwardDir = transform.TransformDirection(dir) * 1000;
+
+		RaycastHit hit;
+		if (Physics.Raycast(cam.transform.position,forwardDir, out hit))
+			print("I'm looking at " + hit.point);
+		else
+			print("I'm looking at nothing!");
+		
+		Debug.DrawRay(transform.position, forwardDir, Color.green);
+	}
+}
